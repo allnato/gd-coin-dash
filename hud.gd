4bc2d21b4446b7329a54c03d0bad4066f3857e62
Allendale Nato <natoallendale@gmail.com>
2024-08-21 12:39:37 +0800

    Add HUD, HUD signals, and coin tweens

---
+++ hud.gd
@@ -0,0 +1,31 @@
+extends CanvasLayer
+
+signal start_game
+
+func update_score(value):
+	$MarginContainer/Score.text = str(value)
+
+func update_time(value):
+	$MarginContainer/Time.text = str(value)
+
+func show_message(text):
+	$Message.text = text
+	$Message.show()
+	$Timer.start()
+
+
+func _on_timer_timeout() -> void:
+	$Message.hide()
+
+
+func _on_start_button_pressed() -> void:
+	$StartButton.hide()
+	$Message.hide()
+	start_game.emit()
+	
+func show_game_over():
+	show_message("Game Over")
+	await $Timer.timeout
+	$StartButton.show()
+	$Message.text = "Coin Dash!"
+	$Message.show()
--- main.gd
+++ main.gd
@@ -14,10 +14,8 @@ func _ready() -> void:
 	screensize = get_viewport().get_visible_rect().size
 	$Player.screensize = screensize
 	$Player.hide()
-	# Start the Game
-	#new_game()
 	
-func _process(delta):
+func _process(_delta):
 	if playing and get_tree().get_nodes_in_group("coins").size() == 0:
 		level += 1
 		time_left += 5
@@ -32,6 +30,9 @@ func new_game():
 	$Player.show()
 	$GameTimer.start()
 	spawn_coins()
+	# Reset HUD values
+	$HUD.update_score(score)
+	$HUD.update_time(time_left)
 	
 func spawn_coins():
 	for i in level + 4:
@@ -40,3 +41,30 @@ func spawn_coins():
 		c.screensize = screensize
 		c.position = Vector2(randi_range(0, screensize.x), randi_range(0, screensize.y))
 		
+
+
+func _on_game_timer_timeout() -> void:
+	time_left -= 1
+	$HUD.update_time(time_left)
+	if time_left <= 0:
+		game_over()
+
+
+func _on_player_hurt() -> void:
+	game_over()
+	
+
+func _on_player_pickup() -> void:
+	score += 1
+	$HUD.update_score(score)
+	
+func game_over():
+	playing = false
+	$GameTimer.stop()
+	get_tree().call_group("coins", "queue_free")
+	$HUD.show_game_over()
+	$Player.die()
+
+
+func _on_hud_start_game() -> void:
+	new_game()
