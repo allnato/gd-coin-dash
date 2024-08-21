690515bef1466521a43eae3da19822a39e2f3cd2
Allendale Nato <natoallendale@gmail.com>
2024-08-21 18:06:32 +0800

    Add powerups

--- main.gd
+++ main.gd
@@ -1,6 +1,7 @@
 extends Node
 
 @export var coin_scene : PackedScene
+@export var powerup_scene : PackedScene
 @export var playtime = 30
 
 var level = 1
@@ -20,6 +21,8 @@ func _process(_delta):
 		level += 1
 		time_left += 5
 		spawn_coins()
+		$PowerUpTimer.wait_time = randf_range(5, 10)
+		$PowerUpTimer.start()
 
 func new_game():
 	playing = true
@@ -63,11 +66,18 @@ func _on_player_hurt() -> void:
 	game_over()
 	
 
-func _on_player_pickup() -> void:
-	score += 1
-	$CoinSound.play()
-	$HUD.update_score(score)
-	
+func _on_player_pickup(type):
+	match type:
+		"coin":
+			$CoinSound.play()
+			score += 1
+			$HUD.update_score(score)
+		"powerup":
+			$PowerUpSound.play()
+			time_left += 5
+			$HUD.update_time(time_left)
+
+
 func game_over():
 	playing = false
 	$GameTimer.stop()
@@ -79,3 +89,10 @@ func game_over():
 
 func _on_hud_start_game() -> void:
 	new_game()
+
+
+func _on_power_up_timer_timeout() -> void:
+	var p = powerup_scene.instantiate()
+	add_child(p)
+	p.screensize = screensize
+	p.position = Vector2(randi_range(0, screensize.x), randi_range(0, screensize.y))
--- player.gd
+++ player.gd
@@ -45,7 +45,10 @@ func die():
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("coins"):
 		area.pickup()
-		pickup.emit()
+		pickup.emit("coin")
+	if area.is_in_group("powerups"):
+		area.pickup()
+		pickup.emit("powerup")
 	if area.is_in_group("obstacles"):
 		hurt.emit()
 		die()
